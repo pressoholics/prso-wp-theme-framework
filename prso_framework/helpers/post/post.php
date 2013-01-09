5a4dca6779b0a05c60e6ac27cbeff0bb2321f81e
Benjamin Moody <emailme@benjaminmoody.com>
2013-01-09 14:12:26 -0500

    New Helper - 'prso_get_related_posts'

--- prso_framework/helpers/post/post.php
+++ prso_framework/helpers/post/post.php
@@ -6,6 +6,8 @@
  *
  * CONTENTS:
  *
+ *	1. do_action('prso_get_related_posts', $args); 
+ *
  *	get_ID_by_slug($page_slug)
  *	get_posts( $args = array() )
  *	loop_category( $parent_cat_slug = null, $args = array() )
@@ -19,9 +21,254 @@ class PostHelper {
 	private $loop_cat_parent_id = null;
 	
 	function __construct() {
-	
+		
+		//Add custom action hooks for post helpers
+ 		$this->custom_action_hooks();
+		
 	}
-
+	
+	/**
+	* add_action
+	* 
+	* Helper to deal with Wordpress add_action requests. Checks to make sure that the action is not
+	* duplicated if a class is instantiated multiple times.
+	* 
+	* @access 	protected
+	* @author	Ben Moody
+	*/
+	private function add_action( $tag = NULL, $method = NULL, $priority = 10, $accepted_args = NULL ) {
+		
+		if( isset($tag,$method) ) {
+			//Check that action has not already been added
+			if( !has_action($tag) ) {
+				add_action( $tag, array($this, $method), $priority, $accepted_args );
+			}
+		}
+		
+	}
+	
+	/**
+	* add_filter
+	* 
+	* Helper to deal with Wordpress add_filter requests. Checks to make sure that the filter is not
+	* duplicated if a class is instantiated multiple times.
+	* 
+	* @access 	protected
+	* @author	Ben Moody
+	*/
+	private function add_filter( $tag = NULL, $method = NULL, $priority = 10, $accepted_args = NULL ) {
+		
+		if( isset($tag,$method) ) {
+			//Check that action has not already been added
+			if( !has_filter($tag) ) {
+				add_filter( $tag, array($this, $method), $priority, $accepted_args );
+			}
+		}
+		
+	}
+	
+	/**
+	* custom_action_hooks
+	* 
+	* Create any custom WP Action Hooks here for post helpers
+	* 
+	* @access 	private
+	* @author	Ben Moody
+	*/
+ 	private function custom_action_hooks() {
+ 		
+ 		/**
+ 		* prso_get_related_posts
+ 		* 	Returns/Echos a ul list of related posts.
+ 		*/
+ 		$this->add_action( 'prso_get_related_posts', 'get_related_posts', 10, 1 );
+ 		
+ 	}
+	
+	/**
+	* get_related_posts
+	* 
+	* do_action('prso_get_related_posts', $args);
+	*
+	* Custom method which returns an ul list of related posts.
+	*
+	* Use in theme via custom Action Hook 'prso_get_related_posts'
+	* 
+	* Use WP_Query args to adjust std query elements such as numberposts.
+	* Also accepts a number of custom args:
+	*
+	* 1. 'relation' - category/tag/category__and__tag/category__or__tag
+	*		Use this to adjust the relationship between tags and categories when
+	*		querying the post relationship, so you want tag/cat to be independent
+	*		or do you define related post as posts which share both cat and tag
+	*		or just on or the other.
+	*
+	* 2. 'no_posts_msg'	- string or NULL
+	*		If you dont need a msg to output when no related posts found, set to NULL
+	*
+	* 3. 'echo' - TRUE/FALSE
+	*		When set to FALSE function will return unordered list html.
+	*
+	* @param	array	$args (see above comments)
+	* @access 	public
+	* @author	Ben Moody
+	*/
+	public function get_related_posts( $args = array() ) {
+		
+		//Init vars
+		global $post;
+		$tags		= array();
+		$tag_ids	= array();
+		$cats		= array();
+		$cat_ids	= array();
+		$query 		= array();
+		$get_posts	= NULL;
+		$related_posts = NULL;
+		$output		= NULL;
+		$defaults = array(
+			'numberposts' => 5, 'offset' => 0,
+			'category' => 0, 'orderby' => 'post_date',
+			'order' => 'DESC', 'include' => array(),
+			'exclude' => array(), 'meta_key' => '',
+			'meta_value' =>'', 'post_type' => 'post',
+			'suppress_filters' => true,
+			'no_posts_msg'	=> 'No related posts',
+			'echo'			=> TRUE,
+			'relation'		=> 'category__and__tag'
+		);
+		
+		//Parse args
+		$query = wp_parse_args( $args, $defaults );
+		
+		//First check the requested relationship between tag and catagories and set the wp_query tax_query arg
+		switch( $query['relation'] ) {
+			case 'category__and__tag':
+				$query['tax_query']['relation'] = 'AND';
+				break;
+			case 'category__or__tag':
+				$query['tax_query']['relation'] = 'OR';
+				break;
+		}
+		
+		//Check if we need categories
+		if( $query['relation'] === 'tag' || $query['relation'] === 'category__and__tag' || $query['relation'] === 'category__or__tag' ) {
+			
+			//Get all post tag id's
+			$tags = wp_get_post_tags($post->ID);
+			
+			//Loop post tags and cache in query var
+			if( !empty($tags) ) {
+				foreach( $tags as $tag ) {
+					if( isset($tag->term_id) ) {
+						$tag_ids[] = $tag->term_id;
+					}
+				}
+				
+				//Cache a wp_query tax_query arg
+				$query['tax_query'][] =	array(
+					'taxonomy'	=> 'post_tag',
+					'field'		=> 'id',
+					'terms'		=> $tag_ids
+				);
+			}
+			
+		}		
+		
+		
+		//Check if we need categories
+		if( $query['relation'] === 'category' || $query['relation'] === 'category__and__tag' || $query['relation'] === 'category__or__tag' ) {
+			
+			//Get all post cat id's
+			$cats = wp_get_post_categories( $post->ID );
+			
+			//Loop post categories and cache in query var
+			if( !empty($cats) ) {
+				foreach( $cats as $cat ) {
+					//Get term data on current cat in loop
+					$term = get_category( $cat );
+					//Cache the terms term_id
+					if( isset($term->term_id) ) {
+						$cat_ids[] = $term->term_id;
+					}
+				}
+				
+				//Cache a wp_query tax_query arg
+				$query['tax_query'][] =	array(
+					'taxonomy' 			=> 'category',
+					'terms' 			=> $cat_ids,
+					'field' 			=> 'term_id',
+					'include_children' 	=> false
+				);
+				
+				//Do not relate posts via the 'uncategorized' category
+				$query['tax_query'][] = array(
+					'taxonomy'	=> 'category',
+					'terms'		=> array( 1 ),
+					'field'		=> 'term_id',
+					'operator'	=> 'NOT IN'
+				);
+			}
+			
+		}
+		
+		//Lets get all related posts using our new query
+		$query['post__not_in'] 	= array( $post->ID );
+		$get_posts 				= new WP_Query;
+		$related_posts 			= $get_posts->query( $query );
+		
+		//Start the html to output unordered list
+		$output = '<ul id="prso-related-posts">';
+		
+		//Loop any posts found and add a list item for each to output html
+		if( !empty($related_posts) ) {
+			
+			foreach( $related_posts as $post ) {
+				//Prepare global post data
+				setup_postdata( $post );
+				//Cache html
+				ob_start();
+				?>
+				<li class="related_post">
+					<a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+				</li>
+				<?php
+				$output.= ob_get_contents();
+				ob_end_clean();
+			}
+			
+		} else {
+			
+			//No related posts found
+			if( isset($query['no_posts_msg']) ) {
+				ob_start();
+				?>
+				<li class="related_post">
+					<?php esc_attr_e( $query['no_posts_msg'] ); ?>
+				</li>
+				<?php
+				$output.= ob_get_contents();
+				ob_end_clean();
+			}
+			
+		}
+		
+		//Reset wp query
+		wp_reset_query();
+		//Reset wp global post
+		wp_reset_postdata();
+		
+		//Close output html
+		$output.= '</ul>';
+		
+		//Detect if to echo or return output
+		if( $query['echo'] === TRUE ) {
+			echo $output;
+		} else {
+			return $output;
+		}
+		
+	}
+	
 	/**
 	* get_ID_by_slug
 	* Shortcut for returning a page ID using it's slug
@@ -270,6 +517,8 @@ class PostHelper {
 	 * The helper uses wp_query to query the posts so simply add you wp_query args into $args and the helper
 	 * will automatically create a 1 post/page pagination loop allowing you to use prev/next post buttons for users
 	 * to loop through a set of posts.
+	 *
+	 * Be sure to declare whether you want the 'next' or 'previous' post usnig the 'direction' key in args array
 	 *
 	 * @param	array	$args - any get_categories args you wish to customize
 	 * @return	array	$_children	- Onject array containing all child/grandchild categories
