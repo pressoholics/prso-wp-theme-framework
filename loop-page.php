27cd54a3f50d4323adc9e939714772ddd7f40352
Benjamin Moody <emailme@benjaminmoody.com>
2013-02-14 15:52:44 -0500

    Moved main content container into loop template file

--- loop-page.php
+++ loop-page.php
@@ -1,43 +1,47 @@
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<div id="main" class="eight columns clearfix" role="main">
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	
-	<header>
+	<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 		
-		<?php do_action( 'prso_orbit_banner' ); ?>
+		<header>
+			
+			<?php do_action( 'prso_orbit_banner' ); ?>
+			
+			<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
 		
-		<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+		</header> <!-- end article header -->
 	
-	</header> <!-- end article header -->
-
-	<section class="post_content clearfix" itemprop="articleBody">
-		<?php the_content(); ?>
-
-	</section> <!-- end article section -->
+		<section class="post_content clearfix" itemprop="articleBody">
+			<?php the_content(); ?>
 	
-	<footer>
-
-		<?php the_tags('<p class="tags"><span class="tags-title">Tags:</span> ', ', ', '</p>'); ?>
+		</section> <!-- end article section -->
 		
-	</footer> <!-- end article footer -->
-
-</article> <!-- end article -->
-
-<?php comments_template(); ?>
-
-<?php endwhile; ?>		
-
-<?php else : ?>
-
-<article id="post-not-found">
-    <header>
-    	<h1>Not Found</h1>
-    </header>
-    <section class="post_content">
-    	<p>Sorry, but the requested resource was not found on this site.</p>
-    </section>
-    <footer>
-    </footer>
-</article>
-
-<?php endif; ?>
+		<footer>
+	
+			<?php the_tags('<p class="tags"><span class="tags-title">Tags:</span> ', ', ', '</p>'); ?>
+			
+		</footer> <!-- end article footer -->
+	
+	</article> <!-- end article -->
+	
+	<?php comments_template(); ?>
+	
+	<?php endwhile; ?>		
+	
+	<?php else : ?>
+	
+	<article id="post-not-found">
+	    <header>
+	    	<h1>Not Found</h1>
+	    </header>
+	    <section class="post_content">
+	    	<p>Sorry, but the requested resource was not found on this site.</p>
+	    </section>
+	    <footer>
+	    </footer>
+	</article>
+	
+	<?php endif; ?>
+	
+</div> <!-- end #main -->
