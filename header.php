7d8fb4a6bf50d165b2d7fa2854488aa58208e29f
BENJAMIN MOODY <hireme@benjaminmoody.com>
2013-05-18 17:55:29 -0400

    Mobile/Tablet nav fixes

--- header.php
+++ header.php
@@ -104,6 +104,6 @@
 				</div>
 				
 				<!-- Mobile nav for deep (Teriary) pages if page has any !-->
-				<div id="mobile-deep-nav-container" class="twelve columns show-for-small">
+				<div id="mobile-deep-nav-container" class="twelve columns show-for-small show-for-medium-portrait">
 					<?php do_action( 'prso_deep_mobile_nav' ); ?>
 				</div>
